e48822cf246cee8b987e16f037416d8e3935699a
TobuTheXd <phamthien332013@gmail.com>
2026-03-05 14:46:59 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+from cli import obfuscate
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+class Script(BaseModel):
+    code:str
+    options:dict=None
+
+
+@app.post("/obfuscate")
+def obfuscate_api(script:Script):
+
+    result = obfuscate(script.code, options=script.options)
+
+    return {"result":result}
