2856f6bab639cf3cb0716fc36e8d8ed3286b44ee
TobuTheXd <phamthien332013@gmail.com>
2026-03-14 12:44:07 +0700

    Delete vm/v4/fragments/junk_variants/junk_a.lua

--- vm/v4/fragments/junk_variants/junk_a.lua
+++
@@ -1,7 +0,0 @@
--- Junk Variant A: Simple math accumulator
-elseif _ST == _S_JUNK1 then
-    _junk1 = (_junk1 + _pc * 31) % 65536
-    _ST = _S_JUNK2
-elseif _ST == _S_JUNK2 then
-    _junk2 = (_junk2 ~ _junk1 + (_ins % 256)) % 65536
-    _ST = _S_DECODE
